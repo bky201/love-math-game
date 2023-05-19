6eb3fe2bd8c62ac60defb454201a1b9b74419025
bky <bekrye@gmail.com>
2023-05-19 22:53:36 +0200

    Add line of code to buttons for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,29 @@ document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit"){
-                checkAnswer();
+        let checkAnswerFunc = checkAnswer;
+        let runGameFunc = runGame;
+        
+        button.addEventListener("click", (function(button, checkAnswerFunc, runGameFunc) {
+          return function() {
+            if (button.getAttribute("data-type") === "submit") {
+              checkAnswerFunc();
             } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
+              let gameType = button.getAttribute("data-type");
+              runGameFunc(gameType);
             }
-        })
-    }
+          };
+        })(button, checkAnswerFunc, runGameFunc));
+      }
 
     document.getElementById("answer-box").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
-    })
+    });
 
-    runGame("addition")
-})
+    runGame("addition");
+});
 /**
  * The main game "loop", called when the script is first
  * loaded and after the user's answer has been processed
